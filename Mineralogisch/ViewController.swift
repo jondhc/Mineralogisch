fefe546a2acfc4194994cb1f1e7ef44b9468b82c
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2018-09-16 01:17:35 -0500

    Initial Commit

---
+++ Mineralogisch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mineralogisch
+//
+//  Created by Jonathan Herrejón on 9/16/18.
+//  Copyright © 2018 Jonathan Herrejón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

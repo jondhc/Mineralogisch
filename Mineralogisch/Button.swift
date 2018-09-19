85a9a742c4e34e0f627fe3ef36eddede6e115252
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2018-09-19 15:13:24 -0500

    Added button class and Outlet collection.

---
+++ Mineralogisch/Button.swift
@@ -0,0 +1,9 @@
+//
+//  Button.swift
+//  Mineralogisch
+//
+//  Created by Jonathan Herrejón on 9/19/18.
+//  Copyright © 2018 Jonathan Herrejón. All rights reserved.
+//
+
+import Foundation
--- Mineralogisch/ViewController.swift
+++ Mineralogisch/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var buttons: [UIButton]!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        for i in buttons{
+            print(i.titleLabel)
+        } //end for
         // Do any additional setup after loading the view, typically from a nib.
     } //end viiewDidLoad
 

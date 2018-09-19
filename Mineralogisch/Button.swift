2192857cbe8046486c266c7c4e4ccf6c5b5db606
ioslab udlap <udlap15@macbook-ios-lab-15.local>
2018-09-19 18:40:56 -0500

    Funcionamiento interno del tablero de juego

--- Mineralogisch/Button.swift
+++ Mineralogisch/Button.swift
@@ -13,11 +13,18 @@ class Button: UIButton {
     static var x: Int!
     static var y: Int!
     
-    static func getX() -> Int{
+    static func getX() -> Int {
         return x
     } //end getX
-    static func getY() -> Int{
+    static func getY() -> Int {
         return y
     } //end getY
+    static func setX(x: Int) {
+        self.x = x
+    }//end setX
+    static func setY(y: Int) {
+        self.y = y
+    }//end setY
     
-} //end Button
+    
+} //end class Button()
--- Mineralogisch/ViewController.swift
+++ Mineralogisch/ViewController.swift
@@ -9,21 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var tablero: [[Int]] = Array(repeating: Array(repeating: 0, count: 6), count: 6)
+    var filaAleatoria: Int
+    var columnaAleatoria: Int
     
     @IBOutlet var buttons: [UIButton]!
     
-
+    @IBAction var startButton(_ sender: UIButton) {
+        for _ in 0...3 { //Numero de minas
+            repeat {
+                filaAleatoria = Int(arc4random_uniform(5))
+                coluAleatoria = Int(arc4random_uniform(5))
+            } while tablero[filaAleatoria][coluAleatoria] == 9
+    
+            tablero[filaAleatoria][coluAleatoria] = 9
+    
+            for fila in max(0,filaAleatoria-1)..<min(6,filaAleatoria+2) {
+                for col in max(0,coluAleatoria-1)..<min(6,coluAleatoria+2) {
+                    if tablero[fila][col] != 9 {
+                        tablero[fila][col] += 1
+                    }
+                }
+            }
+        }//end for
+    
+    }//end startButton
+    
     override func viewDidLoad() {
+        var a = 0
+        var b = 0
+        
         super.viewDidLoad()
+        
         for i in buttons{
+            i.setX(a)
+            i.setY(b)
+            if b < 5 {
+                b += 1
+            } else {
+                b = 0
+                a += 1
+            }
             print(i.titleLabel)
         } //end for
-        // Do any additional setup after loading the view, typically from a nib.
+        
     } //end viiewDidLoad
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     } //end didReceiveMemoryWarning
 
 
